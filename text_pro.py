3fcc91cd9b9b237fb055141a1793e2644e94d1ef
Bhavya <bhavya.hari2002@gmail.com>
2022-10-14 23:04:23 +0530

    first commit

---
+++ text_pro.py
@@ -0,0 +1,25 @@
+from ast import While
+
+
+def sentene_maker(p):
+    introgatives = ("how","why","what","when","where")
+    c= p.capitalize()
+    if p.startswith(introgatives):
+        return "{}?".format(c)
+    else:
+        return "{}.".format(c)
+'''
+print(sentene_maker("how are you"))
+'''
+result=[]
+while True:
+     
+     user_input = input("Say something: ")
+     if user_input == "\end" :
+         break
+     else:
+         result.append(sentene_maker(user_input))
+
+print(" ".join(result))
+
+
